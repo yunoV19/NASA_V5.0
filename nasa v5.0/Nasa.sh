1c0b929ec607242ea041cb66d7837d6a2ba9a717
yunoV19 <93053021+yunoV19@users.noreply.github.com>
2021-10-24 02:23:33 +0800

    Add files via upload

---
+++ nasa v5.0/Nasa.sh
@@ -0,0 +1,182 @@
+#!/usr/bin/bash
+#tools sederhana
+#HACK NASA
+#by YUNO@V19
+clear
+
+
+echo " "
+echo  $blue "<|======================================|>"  |lolcat
+echo  $red    "|         SHELL FILE Nasa              |"  |lolcat
+echo  $red    "|                                      |"  |lolcat
+echo  $red    "| Author        : YUNO@V19             |"  |lolcat
+echo  $red    "| Contact       : JO0KER_              |"  |lolcat
+echo  $red    "| Team Code     : TEAM V19 CREW        |"  |lolcat
+echo  $red    "|    All Member Hacker Grey  Hat MY    |"  |lolcat
+echo  $blue "<|======================================|>"  |lolcat
+sleep 1
+
+echo " "
+echo $green "============== $red MENU $green ================="  |lolcat
+echo $blue "===================================="                |lolcat
+echo $blue "|| $green 1. UPLAOD SHELL               $blue ||"    |lolcat
+echo $blue "|| $green 2. TEBAS INDEX                $blue ||"    |lolcat
+echo $blue "|| $green 3. SQLMAP                     $blue ||"    |lolcat
+echo $blue "|| $green 4. IP DETECTTOR               $blue ||"    |lolcat
+echo $blue "|| $green 0. Exit The Program           $blue ||"    |lolcat
+echo $blue "===================================="                |lolcat
+echo $blue"╭──[Masukkan Pilihan Anda]>"    |lolcat
+read -p"╰───────./HABIS IP KO KNE MASOK KAT FILE FBI!!=•>" gans;
+
+
+
+
+
+if [ $gans = 1 ] || [ $gans = 01 ]
+then
+echo $blue"╭──[Mau Kearah Mana cuk]>"  |lolcat
+read -p"╰───────./Yuno@v19=•>" arah;
+sleep 1
+echo $green"╭──[Nak Shell Design macm mne OI setan!]>"    |lolcat
+read -p"╰───────./Yuno@v19=•>" derajat;
+sleep 1
+echo $red "Loading"      |lolcat
+sleep 1
+echo $blue "10%"         |lolcat
+sleep 1
+echo $green "20%"         |lolcat
+sleep 1
+echo $cyan "30%"          |lolcat
+sleep 1
+echo $purple "40%"        |lolcat
+sleep 1
+echo $blue "50%"           |lolcat
+sleep 1
+echo $red "60%"             |lolcat
+sleep 1
+echo $green "70%"           |lolcat
+sleep 1
+echo $cyan "80%"            |lolcat
+sleep 1
+echo $purple "90%"         |lolcat
+sleep 1
+echo $blue "100%"          |lolcat
+sleep 1
+echo $green "Prosessing Done"      |lolcat
+sleep 1
+echo  $blue "Shell Berhasil Di Upload ke arah $arah sejauh $derajat derajat"        |lolcat 
+fi
+
+if [ $gans = 2 ] || [ $gans = 02 ]
+then
+echo $blue"╭──[LANCAU TENGOK SCREEN BABI!!]>"    |lolcat
+read -p"╰───────./Yuno@v19=•>" arah;
+sleep 1
+echo $green"╭──[LANCAU TENGOK SCREEN BABI!!]>"     |lolcat
+read -p"╰───────./Yuno@v19=•>" derajat;
+sleep 1
+echo $red "Loading"    |lolcat
+sleep 1
+echo $blue "10%"      |lolcat
+sleep 1
+echo $green "20%"     |lolcat
+sleep 1
+echo $cyan "30%"     |lolcat
+sleep 1
+echo $purple "40%"    |lolcat
+sleep 1
+echo $blue "50%"     |lolcat
+sleep 1
+echo $red "60%"      |lolcat
+sleep 1
+echo $green "70%"      |lolcat
+sleep 1
+echo $cyan "80%"       |lolcat
+sleep 1
+echo $purple "90%"    |lolcat
+sleep 1
+echo $blue "100%"      |lolcat
+sleep 1
+echo $green "Prosessing Done"      |lolcat
+sleep 1
+echo  $blue "Shell Berhasil Di Upload ke arah $arah sejauh $derajat derajat"      |lolcat
+fi
+
+if [ $gans = 3 ] || [ $gans = 03 ]
+then
+echo $blue"╭──[LANCAU TENGOK SCREEN BABI!!]>"   |lolcat
+read -p"╰───────./Yuno@v19=•>" arah;
+sleep 1
+echo $green"╭──[LANCAU TENGOK SCREEN BABI!!]>"   |lolcat
+read -p"╰───────./Yuno@v19=•>" derajat;
+sleep 1
+echo $red "Loading"     |lolcat
+sleep 1
+echo $blue "10%"    |lolcat
+sleep 1
+echo $green "20%"    |lolcat
+sleep 1
+echo $cyan "30%"     |lolcat
+sleep 1
+echo $purple "40%"   |lolcat
+sleep 1
+echo $blue "50%"     |lolcat
+sleep 1
+echo $red "60%"      |lolcat
+sleep 1
+echo $green "70%"      |lolcat
+sleep 1
+echo $cyan "80%"      |lolcat
+sleep 1
+echo $purple "90%"    |lolcat
+sleep 1
+echo $blue "100%"    |lolcat
+sleep 1
+echo $green "Prosessing Done"    |lolcat
+sleep 1
+echo  $blue "Shell Berhasil Di Upload ke arah $arah sejauh $derajat derajat"     |lolcat
+fi
+
+if [ $gans = 4 ] || [ $gans = 04 ]
+then
+echo $blue"╭──[LANCAU TENGOK SCREEN BABI!!]>"   |lolcat
+read -p"╰───────./Frozen Flames=•>" arah;
+sleep 1
+echo $green"╭──[LANCAU TENGOK SCREEN BABI!!]>"    |lolcat
+read -p"╰───────./Frozen Flames=•>" derajat;
+sleep 1
+echo $red "Loading"   |lolcat
+sleep 1
+echo $blue "10%"     |lolcat
+sleep 1
+echo $green "20%"    |lolcat
+sleep 1
+echo $cyan "30%"    |lolcat
+sleep 1
+echo $purple "40%"   |lolcat
+sleep 1
+echo $blue "50%"    |lolcat
+sleep 1
+echo $red "60%"    |lolcat
+sleep 1
+echo $green "70%"   |lolcat
+sleep 1
+echo $cyan "80%"    |lolcat
+sleep 1
+echo $purple "90%"   |lolcat
+sleep 1
+echo $blue "100%"    |lolcat
+sleep 1
+echo $green "Prosessing Done"    |lolcat
+sleep 1
+echo  $blue "Shell Berhasil Di Upload ke arah $arah sejauh $derajat derajat"     |lolcat
+fi
+
+if [ $gans = 0 ] || [ $gans = 00 ]
+then
+echo  $red "TAHNIAH IP ANDA DI SAVE KAT FILE FBI "                                          |lolcat
+echo  $blue "TUNGGU JELAH ESOK.."                  |lolcat
+echo  $green "MAMPOS KAU..LAIN KALI BACA DLU SEBELOM GUNE"                                   |lolcat
+echo  $pink "DAN LAGI SATU..TAH NIAH ANDA JADI HACKER YANG BAHALOL!!"						|lolcat
+exit 
+fi
